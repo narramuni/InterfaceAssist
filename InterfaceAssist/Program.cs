aa2c03f3116d39c3a2033de4a24cb79593ef1f8e
narramuni <nmspavan@gmail.com>
2023-10-19 17:37:14 +0530

    Add project files.

---
+++ InterfaceAssist/Program.cs
@@ -0,0 +1,53 @@
+﻿using InterfaceClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        static void runApp()
+        {
+
+
+
+            Teacher teacher = new Teacher();
+            teacher.name = "Tharun";
+            teacher.address = "12/46/2";
+            
+            teacher.DateOfJoining =  DateTime.Today;
+
+            Student student = new Student();
+
+            student.name = "Kumar";
+            student.address = "24/12/2";
+            student.DateOfJoining = DateTime.Today;
+
+
+            OfficeStaff staff = new OfficeStaff();
+
+            staff.name = "Venkat";
+            staff.address = "12/46/6";
+            staff.DateOfJoining = DateTime.Today;
+
+
+
+            Console.WriteLine(teacher.getInfo() + "\n" + teacher.getTypeOfPerson());
+
+            Console.WriteLine(student.getInfo() + "\n" + student.getTypeOfPerson());
+
+            Console.WriteLine(staff.getInfo() + "\n" + staff.getTypeOfPerson());
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
---
+++ InterfaceClassLibrary/OfficeStaff.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceClassLibrary
+{
+    public class OfficeStaff : IPerson
+    {
+        public string name;
+        public string address;
+        public DateTime dateOfJoining;
+
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        private DateTime _dateOfJoining;
+
+        public DateTime DateOfJoining
+        {
+            get { return _dateOfJoining; }
+            set { _dateOfJoining = value; }
+        }
+
+        public string getInfo()
+        {
+            return "Name: " + name + ", Address: " + address + ", Date of Joining: " + DateOfJoining.ToString("MM/dd/yyyy");
+        }
+
+        public string getTypeOfPerson()
+        {
+            return "OfficeStaff";
+        }
+    }
+}
